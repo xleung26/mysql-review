c7afe7958d5cb0287326f2b9d9f8ba374c402999
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-25 12:52:49 -0800

    built models

---
+++ database/models.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+const connection = require("./index.js");
+
+const Todo = connection.define(
+  "todo",
+  {
+    todo: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    list_name: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    }
+  },
+  { timestamps: false }
+);
+
+connection.sync({ force: false });
+
+module.exports = Todo;
