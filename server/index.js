28fe322faf7c2811e3132599b98c54ea3bb37cc1
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-25 11:48:34 -0800

    added routes and controllers

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const parser = require("body-parser");
+const PORT = 3000;
+const routes = require("./routes.js");
+
+const app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../static")));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log("App is listening on PORT", PORT));
